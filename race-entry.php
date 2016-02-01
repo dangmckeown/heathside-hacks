40c363b6e7e1e08c48f726717e62f4fca12c4442
Dan McKeown <dangmckeown@users.noreply.github.com>
2016-02-01 13:56:34 +0000

    Add optional events

--- race-entry.php
+++ race-entry.php
@@ -16,7 +16,8 @@ $eanumber = $_POST['eanumber'];
 $dob = $_POST['dob'];
 $gender = $_POST['gender'];
 
-//race details
+//race details - $contact should be email address for Heathsider responsible for coordinating entries
+$contact = "dan@example.com";
 $distance = $_POST['distance'];
 # $prediction_h = $_POST['hours'];
 $prediction_m = $_POST['minutes'];
@@ -24,7 +25,7 @@ $prediction_s = $_POST['seconds'];
 # $prediction_ms = $_POST['milliseconds'];
 
 /*
-//race details
+//second race details
 $distance2 = $_POST['distance2'];
 # $prediction_h2 = $_POST['hours2'];
 $prediction_m2 = $_POST['minutes2'];
@@ -33,7 +34,7 @@ $prediction_s2 = $_POST['seconds2'];
 */
 
 /*
-//race details
+//third race details
 $distance3 = $_POST['distance3'];
 # $prediction3_h = $_POST['hours3'];
 $prediction_m3 = $_POST['minutes3'];
@@ -41,6 +42,14 @@ $prediction_s3 = $_POST['seconds3'];
 # $prediction_ms3 = $_POST['milliseconds3'];
 */
 
+/*
+//field event (eg for club champs)
+$field = $_POST['field'];
+*/
 
+/*
+//second field event (eg for club champs)
+$field2 = $_POST['field2'];
+*/
 
 ?>
