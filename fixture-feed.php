f1c4107244482ba309a3a14226427dafbf1604b7
Dan McKeown <dangmckeown@users.noreply.github.com>
2018-03-09 16:58:59 +0000

    lose first row

--- fixture-feed.php
+++ fixture-feed.php
@@ -5,18 +5,25 @@ $resource = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vQKlbS5a9jczvCfMhPu
 $fh = fopen($resource,'rb');
 
 while(!feof($fh) && $row = fgetcsv($fh)){
-  ?>
+  //check if first entry has date - if so, include it
+if(preg_match("/\d/",$row[0])){  
+?>
 <div class="fixture-details">
   <?php
 
   echo "<b>{$row[1]}</b> ({$row[2]}) <br />";
-  echo "{$row[0]} | {$row[3]}";
-  echo "<a href='{$row[4]}'>More details &rarr;</a>";
+  echo "{$row[0]} | {$row[3]}<br />";
+  echo "<a href='{$row[4]}'";
+  //do external link if offsite
+  if(preg_match("/^http/"),$row[4]){
+  echo " target='_blank'";
+  }
+  echo ">More details &rarr;</a>";
 
   ?>
 </div>
 <?php
-
+}
 }
 
 fclose($fh);
